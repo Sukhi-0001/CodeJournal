0e7ea0ca205a125fe1d0bd045f0ebe0df88f0544
Sukhi <sukhman.singh734@gmail.com>
2026-04-15 15:40:27 +0530

    solved codeforces again twenty five problem

---
+++ codeforces/CodeforcesExperimentalEducationalRoundVolBITFormulasBlitz/A_AgainTwentyFive.cpp
@@ -0,0 +1,18 @@
+// time-limit: 500
+// problem-url: https://codeforces.com/problemset/problem/630/A
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  long long int n;
+  cin >> n;
+  cout << "25";
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  solve();
+  return 0;
+}
